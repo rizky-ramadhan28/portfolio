1828a554353e806027f3321ae9c6df0e34fc318f
Rizky Ramadhan <freeebook05@gmail.com>
2020-05-26 01:34:25 +0700

    Updates

---
+++ precache-manifest.89ac5ab1e4c653a7724c28adae41c596.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57618f686030317500e2794917905a1a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9522378c4b58ba8a070",
+    "url": "/portfolio/static/css/main.5a54b555.chunk.css"
+  },
+  {
+    "revision": "34ff4acf71567d71c71a",
+    "url": "/portfolio/static/js/2.fb03770d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.fb03770d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9522378c4b58ba8a070",
+    "url": "/portfolio/static/js/main.9b210490.chunk.js"
+  },
+  {
+    "revision": "d6f5cca8b2c2603487a0",
+    "url": "/portfolio/static/js/runtime-main.9b7d8384.js"
+  },
+  {
+    "revision": "a2d4035e88cf88923479c64d12f9bc78",
+    "url": "/portfolio/static/media/envelope-fill.a2d4035e.svg"
+  },
+  {
+    "revision": "c1f41e9f9f1f269ffd4e5dfea3b975a4",
+    "url": "/portfolio/static/media/profile.c1f41e9f.jpg"
+  }
+]);
