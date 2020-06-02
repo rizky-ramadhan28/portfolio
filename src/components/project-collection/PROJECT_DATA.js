d4e2ef870ddf18c49075f2ca62947c9c2b1ce5ef
Rizky Ramadhan <freeebook05@gmail.com>
2020-06-03 05:04:27 +0700

    Add new project.

--- src/components/project-collection/PROJECT_DATA.js
+++ src/components/project-collection/PROJECT_DATA.js
@@ -7,6 +7,14 @@ const PROJECT_DATA = [
       "https://images.unsplash.com/photo-1586769852836-bc069f19e1b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     date: "29 Mei 2020",
   },
+  {
+    id: "luyskwquaw",
+    name: "Simple Blog",
+    projectUrl: "https://rizky-ramadhan28.github.io/simple-blog/",
+    imageUrl:
+      "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    date: "03 Juni 2020",
+  },
 ];
 
 export default PROJECT_DATA;
